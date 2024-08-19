a09bd09afc8bc8adf3dc5ce5a312a946cd4279a6
catherine-rocha <cathyrocha18@hotmail.com>
2024-08-19 13:33:08 -0300

    stopPropagation

--- eventos.js
+++ eventos.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-
+    let botón = document.querySelector("input")
     let div = document.querySelector("div");
 
     function saludarDiv() {
         alert("Hola!Soy el div");
     };
 
+    botón.addEventListener("click",(event)=> {
+        alert("Hola!");
+        event.stopPropagation();
+    });
+    
     div.addEventListener("click",saludarDiv);
+
 
 });
